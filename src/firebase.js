b600c965eac12f8bc722f09042b04985378f8bf6
LEFIFENE Hamza <lefifeneh83@gmail.com>
2026-03-04 01:52:02 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,21 @@
-// src/firebase.js
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// إعدادات Firebase الخاصة بمشروعك
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "API_KEY_HERE",
-  authDomain: "PROJECT_ID.firebaseapp.com",
-  projectId: "PROJECT_ID",
-  storageBucket: "PROJECT_ID.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID",
+  apiKey: "AIzaSyBTR1psf3Gtrrn9-PdWIUI--xgCL2Nwa6I",
+  authDomain: "ta-line.firebaseapp.com",
+  projectId: "ta-line",
+  storageBucket: "ta-line.firebasestorage.app",
+  messagingSenderId: "853675904478",
+  appId: "1:853675904478:web:cec17cb3f903b3db4a214f",
+  measurementId: "G-B067XD6M0G"
 };
 
-// تهيئة التطبيق
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// تهيئة Authentication
-export const auth = getAuth(app);
+const analytics = getAnalytics(app);
